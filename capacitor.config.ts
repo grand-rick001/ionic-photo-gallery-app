641b3482d46491d5c79567f0a3d4235b26ff6063
Patrick Murimi <kabugap02@gmail.com>
2024-02-06 12:24:14 +0300

    feat: runs on mobile

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,12 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'photo-gallery',
+  appId: 'grand-rick',
+  appName: 'Rick and Morty Photo Gallery',
   webDir: 'www',
   server: {
-    androidScheme: 'https'
-  }
+    androidScheme: 'https',
+  },
 };
 
 export default config;
